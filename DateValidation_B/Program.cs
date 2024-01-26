8bc971ae236a939ce72bd784ee96927a97eb689b
Ivan Demidov <ivan.demidov00@mail.ru>
2024-01-26 17:26:44 +0500

    Task - B

---
+++ DateValidation_B/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+var testCasesCount = Convert.ToInt32(Console.ReadLine());
+const string dateFormat = "d M yyyy";
+for (var i = 0; i < testCasesCount; i++)
+{
+    var testCase = Console.ReadLine()?.ToCharArray();
+    Console.WriteLine(
+        DateTime.TryParseExact(testCase, dateFormat, CultureInfo.CurrentCulture, DateTimeStyles.None, out _)?
+        "YES":
+        "NO");
+}
